bf2de6569f24b6acade195a268f234482fc2248f
laurarengifo09 <68392532+laurarengifo09@users.noreply.github.com>
2023-03-22 12:00:51 -0500

    No se que changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:odisserr/views/screens/Perfil.dart';
 import 'package:odisserr/views/screens/auth/welcome_page.dart';
 import 'package:odisserr/views/screens/profile.dart';
 
-void main() {
+import 'package:flutter/widgets.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: FirebaseOptions());
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.black,
     systemNavigationBarIconBrightness: Brightness.light,
@@ -21,6 +26,6 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Open Sans',
           scaffoldBackgroundColor: Colors.white,
         ),
-        home: Profile());
+        home: Perfil());
   }
 }
