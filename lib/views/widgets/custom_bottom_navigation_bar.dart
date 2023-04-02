18f157811f3fbf2d26008eacb1853096bf249ced
JDEPAP2 <josedavidescobarprada@gmail.com>
2023-04-01 19:37:46 -0500

    a

--- lib/views/widgets/custom_bottom_navigation_bar.dart
+++ lib/views/widgets/custom_bottom_navigation_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:odisserr/views/utils/AppColor.dart';
+
 
 // ignore: must_be_immutable
 class CustomBottomNavigationBar extends StatefulWidget {
